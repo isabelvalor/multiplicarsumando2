2bdde20da4b01e1afece2598316691e1b169b146
vesprada <vesprada@localhost>
2014-12-18 20:46:07 +0100

    saok

---
+++ src/multiplicadorsumando/Multiplicadorsumando.java
@@ -0,0 +1,16 @@
+package multiplicadorsumando;
+import java.util.Scanner;
+public class Multiplicadorsumando {
+
+    public static void main(String[] args) {
+        int numero,multiplicador,contador,igualador;
+        Scanner introducir=new Scanner(System.in);
+        System.out.println("Introduce el numero");
+        numero=introducir.nextInt();
+        System.out.println("introducel el multiplicador");
+        multiplicador=introducir.nextInt();
+        do{
+    }while();
+    
+}
+}
