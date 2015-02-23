0b336642487e6802c5044317fca0baa13dcb2ebe
isabelvalor <isabelvalor@hotmail.com>
2015-02-23 19:35:40 +0100

    Update Multiplicadorsumando.java

--- src/multiplicadorsumando/Multiplicadorsumando.java
+++ src/multiplicadorsumando/Multiplicadorsumando.java
@@ -10,7 +10,7 @@ public class Multiplicadorsumando {
         System.out.println("introducel el multiplicador");
         multiplicador=introducir.nextInt();
         do{
-    }while();// hola soy isa
+    }while();// hola soy isa valor
     
 }
 }
